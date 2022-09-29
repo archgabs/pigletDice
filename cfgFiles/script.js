6188e169034eec370cd1b704619d668c2c671daa
João Gabriel Peixoto Rodrigues <j.gabriel.2020@hotmail.com>
2022-09-29 01:21:15 -0300

    Major Update
    A complete re-made code, starting through the same base, logic maintained but different aspects as using new functions where updated.

--- cfgFiles/script.js
+++ cfgFiles/script.js
@@ -1,4 +1,3 @@
-// This is the 1st Version of the code, the final and refactored is on WiP.
 // Button Related
 const btnRollEl = document.querySelector('.btn-roll');
 const btnHoldEl = document.querySelector('.btn-hold');
@@ -11,88 +10,91 @@ const currentEl1 = document.querySelector('.current--1');
 const scoreEl0 = document.querySelector('.score--0');
 const scoreEl1 = document.querySelector('.score--1');
 // Score Variables
-let score = [0, 0];
-// Player Related
-let isPlayerOne = true;
-let isGameRunning = true;
+let score, currentPlayer, isGameRunning, currentScore;
+// Side Related
 const side0 = document.querySelector('.side0');
 const side1 = document.querySelector('.side1');
-// Roll Button Event
-btnRollEl.addEventListener('click', () => {
-    if (isGameRunning) {
-        let randomDice = Math.trunc(Math.random() * 6 + 1);
-        dice.classList.remove('hidden');
-        dice.src = `dice-${randomDice}.png`;
-
-        if (randomDice !== 1) {
-            if (isPlayerOne) {
-                // If isPlayerOne === true
-                score[0] += randomDice;
-                currentEl0.textContent = score[0];
-            } else {
-                // If isPlayerOne === flase
-                score[1] += randomDice;
-                currentEl1.textContent = score[1];
-            }
-        } else {
-            if (isPlayerOne) {
-                score[0] = 0;
-                currentEl0.textContent = 0;
-                side0.classList.toggle('activePlayer');
-                side1.classList.toggle('activePlayer');
-
-                isPlayerOne = false;
-            } else {
-                side0.classList.toggle('activePlayer');
-                side1.classList.toggle('activePlayer');
-                score[1] = 0;
-                currentEl1.textContent = 0;
-                isPlayerOne = true;
-            }
-        }
-    }
-});
-// Hold Button Event
-btnHoldEl.addEventListener('click', () => {
-    if (isPlayerOne) {
-        if (score[0] >= 100) {
-            side0.classList.add('winnerPlayer');
-            isGameRunning = false;
-        } else {
-            isPlayerOne = false;
-            currentEl0.textContent = 0;
-            scoreEl0.textContent = score[0];
-            side0.classList.toggle('activePlayer');
-            side1.classList.toggle('activePlayer');
-        }
-    } else {
-        if (score[1] >= 100) {
-            isGameRunning = false;
-            side1.classList.add('winnerPlayer');
-        } else {
-            isPlayerOne = true;
-            currentEl1.textContent = 0;
-            scoreEl1.textContent = score[1];
-            side0.classList.toggle('activePlayer');
-            side1.classList.toggle('activePlayer');
-        }
-    }
-});
-// Restart Button Event
-btnRestartEl.addEventListener('click', () => {
+const startGame = () => {
+    // Reseting Script Variables
+    score = [0, 0];
+    currentPlayer = 0;
     isGameRunning = true;
-    isPlayerOne = true;
-
-    score[0] = 0;
-    score[1] = 0;
+    currentScore = 0;
+    /*
+     Reseting to initial state, meaning that removing classes from unecessary angles and setting to 0 score and current scores...
+    */
     side0.classList.add('activePlayer');
-    side1.classList.remove('activePlayer');
+    side1.classList.remove('playerActive');
 
     side0.classList.remove('winnerPlayer');
     side1.classList.remove('winnerPlayer');
 
-    currentEl0.textContent = 0;
-    currentEl1.textContent = 0;
     scoreEl0.textContent = 0;
     scoreEl1.textContent = 0;
+
+    currentEl0.textContent = 0;
+    currentEl1.textContent = 0;
+};
+// Starting Game as Script Loads
+startGame();
+
+const switchPlayer = () => {
+    // Current Roll Stack goes 0 dinamicaly
+    document.querySelector(`.current--${currentPlayer}`).textContent = 0;
+    // Swithing sides with .toggle
+    side0.classList.toggle('activePlayer');
+    side1.classList.toggle('activePlayer');
+    // Switching Player
+    currentPlayer === 0 ? (currentPlayer = 1) : (currentPlayer = 0);
+    // Reseting Current Score (to reuse.)
+    currentScore = 0;
+};
+
+btnRollEl.addEventListener('click', () => {
+    if (isGameRunning) {
+        // Generating Dice and Removing Hidden Class
+        const randomDice = Math.trunc(Math.random() * 6 + 1);
+        dice.classList.remove('hidden');
+        dice.src = `dice-${randomDice}.png`;
+
+        if (randomDice !== 1) {
+            currentScore += randomDice;
+            // Dinamically Alocating Current Score
+            document.querySelector(`.current--${currentPlayer}`).textContent =
+                currentScore;
+        } else {
+            switchPlayer();
+        }
+    }
+});
+
+btnHoldEl.addEventListener('click', () => {
+    if (isGameRunning) {
+        // Adding to Score Array the Current Score Via CurrentPlayer Variable
+        score[currentPlayer] += currentScore;
+        document.querySelector(`.score--${currentPlayer}`).textContent =
+            score[currentPlayer];
+
+        if (score[currentPlayer] >= 100) {
+            // Stoping the game to block buttons and other functionalities
+            isGameRunning = false;
+            // Adding WinnerPlayer to the side who got 100p
+            document
+                .querySelector(`.side${currentPlayer}`)
+                .classList.add('winnerPlayer');
+            // Removing Current Side activePlayer Status
+            document
+                .querySelector(`.side${currentPlayer}`)
+                .classList.remove('activePlayer');
+            // Hiding Dice
+            dice.classList.add('hidden');
+        } else {
+            // Switching In Case Current Score isnt === 100
+            switchPlayer();
+        }
+    }
+});
+
+btnRestartEl.addEventListener('click', () => {
+    startGame();
 });
